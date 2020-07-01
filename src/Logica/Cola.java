67854a622f99bd070c195c2b3cad6b01048b833b
nicolaslopez99 <42530363+nicolaslopez99@users.noreply.github.com>
2020-07-01 16:15:16 -0500

    Add files via upload

--- src/Logica/Cola.java
+++ src/Logica/Cola.java
@@ -1,23 +1,142 @@
 package Logica;
 
+import java.util.ArrayList;
+
 public class Cola {
+	private int procesos = 0;
 	private Nodo raiz = new Nodo();
 	private int id = 1;
-	private int llegada = 0;
+	private int rafagaTotal = 0;
+	private int numProcesos = 0;
+	public ArrayList<String[]> Data = new ArrayList<String[]>();
 
 	public Cola() {
 		raiz.rafaga = -1;
 		raiz.id = -1;
 		raiz.sig = raiz;
 	}
+	
 
-	public void insertar(int ra) {
+	public int getProcesos() {
+		return procesos;
+	}
+
+
+	public void setProcesos(int procesos) {
+		this.procesos = procesos;
+	}
+
+
+	public Nodo getRaiz() {
+		return raiz;
+	}
+
+
+	public void setRaiz(Nodo raiz) {
+		this.raiz = raiz;
+	}
+
+
+	public boolean isEmpty() {
+		if (this.raiz.sig == this.raiz) {
+			return true;
+		}
+		return false;
+	}
+
+	public void mostrar() {
+		Nodo aux = raiz;
+		while (aux.sig != raiz) {
+			aux = aux.sig;
+			System.out.println(aux.id + " | " + aux.tllegada + " | " + aux.rafaga + " | " + aux.tcomienzo + " | "
+					+ aux.tfinal + " | " + aux.tretorno + " | " + aux.tespera);
+		}
+	}
+
+	public String[] Data() {
+
+		Nodo aux = raiz;
+		while (aux.sig != raiz) {
+			aux = aux.sig;
+		}
+		String[] data = { String.valueOf(aux.id), String.valueOf(aux.tllegada), String.valueOf(aux.rafaga),
+				String.valueOf(aux.tcomienzo), String.valueOf(aux.tfinal), String.valueOf(aux.tretorno),
+				String.valueOf(aux.tespera) };
+		return data;
+	}
+
+	public ArrayList<String[]> allData() {
+		Nodo aux = raiz;
+		while (aux.sig != raiz) {
+			aux = aux.sig;
+			String[] process = { String.valueOf(aux.id), String.valueOf(aux.tllegada), String.valueOf(aux.rafaga),
+					String.valueOf(aux.tcomienzo), String.valueOf(aux.tfinal), String.valueOf(aux.tretorno),
+					String.valueOf(aux.tespera) };
+			Data.add(process);
+		}
+
+		return Data;
+	}
+
+	public int getrafagaTotal() {
+		return rafagaTotal;
+	}
+
+	public int getnumProcesos() {
+		return numProcesos;
+	}
+	void recorrer() {
+		
+	}
+
+	void ordenar() {
+		Nodo aux;
+		while (true) {
+			aux = raiz.sig;
+			boolean ordenar = false;
+			if (procesos <= 1) {
+				break;
+			}
+			for (int i = 1; i < procesos; i++) {
+				if (aux.rafaga > aux.sig.rafaga) {
+					if (aux.sig.rafaga != -1) {
+						Nodo padre = buscarp(aux);
+						padre.sig = aux.sig;
+						Nodo aux1 = aux.sig.sig;
+						aux.sig = aux1;
+						padre.sig.sig = aux;
+						ordenar = true;
+					}
+				}
+				aux = aux.sig;
+				System.out.println(procesos);
+			}
+			System.out.println();
+			System.out.println(ordenar);
+			if (ordenar == false) {
+				break;
+			}
+		}
+	}
+
+	Nodo buscarp(Nodo actual) {
+		Nodo padre = raiz;
+		Nodo aux = raiz;
+		while (aux.sig != actual) {
+			aux = aux.sig;
+			padre = aux;
+		}
+		return padre;
+	}
+
+	public void insertar(int ra, int tllegada) {
+		rafagaTotal += ra;
+		numProcesos++;
 		Nodo nuevo = new Nodo();
 		nuevo.rafaga = ra;
 		nuevo.id = id;
-		nuevo.tllegada = llegada;
+		nuevo.tllegada = tllegada;
 		id++;
-		llegada++;
 
 		if (raiz.sig == raiz) {
 			nuevo.tcomienzo = 0;
@@ -40,31 +159,35 @@ public class Cola {
 			paux.sig = nuevo;
 			nuevo.sig = raiz;
 		}
+		procesos++;
 	}
 
-	public boolean isEmpty() {
-		if (this.raiz.sig == this.raiz) {
-			return true;
+	public void atender() {
+		Nodo at = raiz.sig;
+		if (at == raiz) {
+			System.out.println("No hay nadie en la cola");
+		} else {
+			if (at.rafaga <= 5) {
+				raiz.sig = at.sig;
+				at.sig = null;
+				System.out.println("Cliente atendido");
+			} else {
+				Nodo aux = raiz;
+				Nodo paux = raiz;
+				while (aux.sig != raiz) {
+					aux = aux.sig;
+					paux = aux;
+				}
+				raiz.sig = at.sig;
+				paux.sig = at;
+				at.sig = raiz;
+				at.rafaga -= 5;
+			}
 		}
-		return false;
-	}
-
-	public void mostrar() {
-		Nodo aux = raiz;
-		while (aux.sig != raiz) {
-			aux = aux.sig;
-			System.out.println(aux.id + " | " + aux.tllegada+ " | " + aux.rafaga+ " | " + aux.tcomienzo+ " | " + aux.tfinal+ " | " + aux.tretorno+ " | " + aux.tespera);
-		}
-	}
-
-	public String dibujar() {
-		String colaActual = ": |";
-		Nodo aux = raiz;
-		while (aux.sig != raiz) {
-			aux = aux.sig;
-			colaActual += aux.rafaga + " | ";
-		}
-		return colaActual;
+		System.out.println();
+		mostrar();
+		System.out.println();
+		procesos -= 1;
 	}
 
 }
--- src/Logica/main.java
+++ src/Logica/main.java
@@ -5,13 +5,17 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Cola cola1 = new Cola ();
-		cola1.insertar(2);
-		cola1.insertar(8);
-		cola1.insertar(4);
-		cola1.insertar(9);
-		cola1.insertar(5);
+
+		cola1.insertar(8,0);
+		cola1.insertar(4,1);
+		cola1.insertar(9,2);
+		cola1.insertar(5,3);
+		cola1.insertar(2,4);
 		
+	//	cola1.ordenar();
+
 		cola1.mostrar();
+
 	}
 
 }
